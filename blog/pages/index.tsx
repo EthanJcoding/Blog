b946f0e6d7d491ba294d3b21ef42e79311714211
ethanJcoding <ethan.95j@gmail.com>
2023-04-16 19:29:51 +0900

    Design: Header update

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -1,14 +1,10 @@
 import { Button } from "@/component/atoms/Button/Button";
+import { Header } from "@/component/organisms/Header/Header";
 
 export default function Home() {
   return (
     <>
-      <main>
-        <div>여기는 홈입니다</div>
-      </main>
-      <Button size="default" intent="default">
-        any text gkgkgkgkgk
-      </Button>
+      <Header></Header>
     </>
   );
 }
