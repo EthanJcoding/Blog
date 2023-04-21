abcc924ac3b1152d96c2ce8ecbb5cd2720e88a87
ethanJcoding <ethan.95j@gmail.com>
2023-04-21 19:25:53 +0900

    Design: Paragraph atom 스타일 변경 및 프사 public 폴더에 추가

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/component/atoms/Button/Button";
-import Icon from "@/component/atoms/Icon/Icon";
-import { Header } from "@/component/organisms/Header/Header";
+import { IconButton } from "@/component/atoms/Button/Button.stories";
+import { Profile } from "@/component/molecules/Profile/Profile";
 
 export default function Home() {
   return (
-    <>
-      <Header></Header>
-      <Icon icon="FaGithubSquare" size="m"></Icon>
-    </>
+    <div className="flex h-full w-full max-w-[428px] flex-1 flex-col p-6 py-12 xl:max-w-[1728px] xl:flex-row xl:p-16">
+      <Profile />
+    </div>
   );
 }
