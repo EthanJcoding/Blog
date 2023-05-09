32445abd7efd1f71a17a82faa56fab2d068520ef
ethanJcoding <ethan.95j@gmail.com>
2023-05-10 00:22:41 +0900

    Fix: 테스트파일 에러 정리

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,13 @@
-import { useFetchNotionData } from "@/hooks/useFetchNotionData/useFetchNotionData";
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const ServerSideNotionData = async (req, res) => {
-  const data = await useFetchNotionData();
-
-  res.status(200).json(data);
+type Data = {
+  name: string;
 };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: "John Doe" });
+}
