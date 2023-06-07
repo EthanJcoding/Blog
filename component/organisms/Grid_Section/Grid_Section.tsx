ed5cac5f1c9ce1e527878bbbb2658e6e3ff78a50
ethanJcoding <ethan.95j@gmail.com>
2023-06-07 17:41:43 +0900

    Design: 그리드 section 레이아웃 분기 추가

--- component/organisms/Grid_Section/Grid_Section.tsx
+++ component/organisms/Grid_Section/Grid_Section.tsx
@@ -1,7 +1,5 @@
 import { Git_Status } from "@/component/atoms/Git_Status/Git_Status";
 import { useRouter } from "next/router";
-import img_wave from "../../../public/img-wave.svg";
-import Image from "next/image";
 
 const Grid_Section = ({ children }: { children: JSX.Element }) => {
   const router = useRouter();
@@ -9,19 +7,24 @@ const Grid_Section = ({ children }: { children: JSX.Element }) => {
   const shouldHideGitStatus = query === "/" ? false : true;
 
   return (
-    <>
-      <div className="relative xl:w-[820px] xl:flex-none ">
+    <div className="relative xl:w-[820px] xl:flex-none ">
+      {!shouldHideGitStatus && (
         <div className="h-40 w-full xl:w-[820px] absolute overflow-hidden rounded-t-3xl">
           <div className="wave2 animate-wave absolute opacity-90"></div>
-          <div className="wave animate-wave absolute opacity-70"></div>
+          <div className="wave animate-wave absolute opacity-80"></div>
         </div>
-        <div className="h-40 mb-10"></div>
-        {!shouldHideGitStatus && <Git_Status />}
-        <div className="grid grid-cols-2 grid-rows-8 xlg:grid-cols-8 xlg:grid-rows-8 gap-10 place-items-center min-h-[784px] ">
-          {children}
-        </div>
+      )}
+
+      {!shouldHideGitStatus && (
+        <>
+          <div className="h-40 mb-10"></div>
+          <Git_Status />
+        </>
+      )}
+      <div className="grid grid-cols-2 grid-rows-8 xlg:grid-cols-8 xlg:grid-rows-8 gap-10 place-items-center min-h-[784px] ">
+        {children}
       </div>
-    </>
+    </div>
   );
 };
 
