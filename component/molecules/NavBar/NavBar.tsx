2696fcb22c22e78b1165c78509a224d4e642e4dd
ethanJcoding <ethan.95j@gmail.com>
2023-07-05 15:41:51 +0900

    Refactor: NavBar 리펙토링

--- component/molecules/NavBar/NavBar.tsx
+++ component/molecules/NavBar/NavBar.tsx
@@ -1,11 +1,7 @@
 import { Button } from "@/component/atoms/Button/Button";
 
 const NavBar = () => {
-  return (
-    <div className="mb-10 px-4 xl:mb-0 xl:mr-20 xl:px-0">
-      <Button icon="RiMenuUnfoldLine" intent="transparent" size="sm"></Button>
-    </div>
-  );
+  return <div className="mb-10 px-4 xl:mb-0 xl:mr-20 xl:px-0"></div>;
 };
 
 export { NavBar };
