efc643f2529eb9a3d9c4fd4f4db49d1d1d12301e
ethanJcoding <ethan.95j@gmail.com>
2023-07-21 23:27:20 +0900

    Design: Grid_Section isFolded 상태일때 height 값 조정

--- services/data/index.tsx
+++ services/data/index.tsx
@@ -74,6 +74,7 @@ export const contentsForMainPage = [
         thumbnail:
           "https://velog.velcdn.com/images/dnr6054/post/21e308c6-106e-46cc-aba0-c01191af3e3f/image.png",
       },
+      {},
     ],
   },
 ];
