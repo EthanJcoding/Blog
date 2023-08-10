65517e2a248d590de79265e522b713c0cee2f99c
ethanJcoding <ethan.95j@gmail.com>
2023-08-10 18:03:05 +0900

    Refactor: archive page css 리펙토링

--- component/organisms/Archive_Section/Archive_Section.tsx
+++ component/organisms/Archive_Section/Archive_Section.tsx
@@ -1,19 +1,7 @@
-import { useGenerationStore } from "services";
-
 const Archive_Section = ({ children }: { children: JSX.Element }) => {
-  const { isFolded } = useGenerationStore();
-
   return (
-    <div className="w-full">
-      {isFolded ? (
-        <div className="flex flex-col xlg:flex-row w-full">
-          <div className="grid gap-10">{children}</div>
-        </div>
-      ) : (
-        <div className="flex flex-col xlg:flex-row w-full">
-          <div className="grid gap-10 ">{children}</div>
-        </div>
-      )}
+    <div className="flex flex-col xlg:flex-row w-full justify-center">
+      <div className="grid gap-10">{children}</div>
     </div>
   );
 };
--- pages/archive/index.tsx
+++ pages/archive/index.tsx
@@ -1,23 +1,11 @@
 import { ArchiveGrid, Layout } from "component/molecules";
-import { GetStaticProps } from "next";
-import {
-  allContents,
-  type Content as ContentType,
-} from "contentlayer/generated";
 import { Archive_Section } from "component/organisms";
 
-export const getStaticProps: GetStaticProps = async () => {
-  return { props: { allContents } };
-};
-export default function archive({
-  allContents,
-}: {
-  allContents: ContentType[];
-}) {
+export default function archive({}) {
   return (
     <Layout>
       <Archive_Section>
-        <ArchiveGrid contents={allContents} />
+        <ArchiveGrid />
       </Archive_Section>
     </Layout>
   );
